3758d11618612d73dcc5e6f924fa114c887d2655
Jenaro Calviño <jen.calvineo@gmail.com>
2021-12-15 12:37:10 -0300

    feat(hook): add contact if its a vcard

--- app/routes/webhook.tsx
+++ app/routes/webhook.tsx
@@ -24,10 +24,42 @@ export let loader: LoaderFunction = async () => {
   return db.telegramLog.findMany();
 };
 
+function getVcardValue(str: string) {
+  return str.slice(str.indexOf(":") + 1);
+}
+
 export let action: ActionFunction = async ({ request }) => {
   const body = await request.json();
   const id = Number(body.update_id);
   const content = JSON.stringify(body.message);
+  const file_id = body?.message?.document?.file_id;
+  const fileName = body?.message?.document?.file_name;
+
+  const getFileUrl = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT}/getFile?file_id=${file_id}`;
+
+  const fileResponse = await fetch(getFileUrl).then((blob) => blob.json());
+  const filePath = fileResponse?.result?.file_path;
+
+  if (!!fileResponse?.ok && !!filePath) {
+    const blob = await fetch(
+      `https://api.telegram.org/file/bot${process.env.TELEGRAM_BOT}/${filePath}`
+    ).then((res) => res.blob());
+    const buffer = await blob.text();
+
+    const parts = buffer.split(/\n/);
+    const [, , , , name, title, role, tel] = parts;
+    await db.trader.upsert({
+      where: {
+        number: getVcardValue(tel),
+      },
+      create: {
+        number: getVcardValue(tel),
+        name: getVcardValue(name),
+        trade: getVcardValue(title) || getVcardValue(role),
+      },
+      update: {},
+    });
+  }
 
   await db.telegramLog.upsert({
     where: { id },
@@ -39,6 +71,7 @@ export let action: ActionFunction = async ({ request }) => {
       content,
     },
   });
+
   return json(
     {},
     {
