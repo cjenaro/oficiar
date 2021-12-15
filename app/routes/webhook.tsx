02c5d0b9eb792c0a83e68e749ebf1f3905374e3a
Jenaro Calviño <jen.calvineo@gmail.com>
2021-12-15 10:54:45 -0300

    fix(webhook): only get message once

--- app/routes/webhook.tsx
+++ app/routes/webhook.tsx
@@ -2,9 +2,15 @@ import { ActionFunction, redirect } from "remix";
 import { db } from "~/db";
 
 export let action: ActionFunction = async ({ request }) => {
-  db.telegramLog.create({
-    data: {
-      content: await request.text(),
+  const body = Object.fromEntries(new URLSearchParams(await request.text()));
+  const id = Number(body.update_id);
+
+  await db.telegramLog.upsert({
+    where: { id },
+    update: {},
+    create: {
+      id,
+      content: JSON.stringify(body.message),
     },
   });
   return redirect("/");
